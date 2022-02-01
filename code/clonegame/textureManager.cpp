4dd99929b68349593fc443481e680c6eb42d7b22
Aureliano <awsidc@gmail.com>
2022-02-01 10:39:16 -0800

    Add texture manager

--- code/clonegame/textureManager.cpp
+++ code/clonegame/textureManager.cpp
@@ -1,9 +1,11 @@
 #include "textureManager.h"
 
-
 SDL_Texture* TextureManager::LoadTexture(const char* texture)
 {
 	SDL_Surface* tempSurface = IMG_Load(texture);
+  if (tempSurface == NULL) {
+    std::cout << "Failed to load image surface" << IMG_GetError();
+  }
 	SDL_Texture* tex = SDL_CreateTextureFromSurface(mRenderer, tempSurface);
 	SDL_FreeSurface(tempSurface);
 	
@@ -17,6 +19,23 @@ void TextureManager::RenderTexture(SDL_Texture *tex, int x, int y, int w, int h)
 	dst.w = w;
 	dst.h = h;
 	SDL_RenderCopy(mRenderer, tex, NULL, &dst);
+}
+
+void TextureManager::ClipTexture(SDL_Texture *tex, float x, float y, int w, int h, int p_x, int p_y, int p_w, int p_h) {
+	SDL_Rect src;
+  int tex_w, tex_h;
+	SDL_QueryTexture(tex, NULL, NULL, &tex_w, &tex_h);
+  src.x = x; 
+  src.y = y;
+  src.w = w;
+  src.h = h;
+
+	SDL_Rect dst;
+	dst.x = p_x;
+	dst.y = p_y;
+	dst.w = p_w;
+	dst.h = p_h;
+	SDL_RenderCopy(mRenderer, tex, &src, &dst);
 }
 
 void TextureManager::RenderTexture(SDL_Texture *tex, int x, int y){
